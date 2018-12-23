5146618c197db3eb3e2b8328c5eb56440787af2b
Rodrigo Pinheiro <dap.rodrigo@gmail.com>
2018-12-23 00:49:42 -0300

    Create basic requests

---
+++ lib/exorthanc.ex
@@ -0,0 +1,18 @@
+defmodule Exorthanc do
+  @moduledoc """
+  Documentation for Exorthanc.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exorthanc.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exorthanc_test.exs
@@ -0,0 +1,8 @@
+defmodule ExorthancTest do
+  use ExUnit.Case
+  doctest Exorthanc
+
+  test "greets the world" do
+    assert Exorthanc.hello() == :world
+  end
+end
