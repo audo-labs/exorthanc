00de28d4153377527dcc4f5fe4efb75ce37f38a0
Rodrigo Pinheiro <dap.rodrigo@gmail.com>
2019-01-02 12:27:57 -0300

    Put compression parameter into options

--- lib/exorthanc/download.ex
+++ lib/exorthanc/download.ex
@@ -23,14 +23,14 @@ defmodule Exorthanc.Download do
        ['/tmp/1.2.840.113619.2.22.288.1.14234.20161124.245137/0.dcm',
         '/tmp/1.2.840.113619.2.22.288.1.14234.20161124.245137/1.dcm']
   """
-  def study(base_url, studyInstanceUid, compression \\ false, hackney_opts \\ []) do
+  def study(base_url, studyInstanceUid, options \\ []) do
     tmp_dir = "#{System.tmp_dir()}/#{studyInstanceUid}"
     with :ok <- File.mkdir_p!(tmp_dir),
-         {:ok, resp} <- Retrieve.study(base_url, studyInstanceUid, hackney_opts),
+         {:ok, resp} <- Retrieve.study(base_url, studyInstanceUid, options),
          boundary when not is_nil(boundary) <- resp.headers |> extract_boundary,
          {:ok, parts} = :hackney_multipart.decode_form(boundary, resp.body)
     do
-      dicom_stream(parts, tmp_dir, compression)
+      dicom_stream(parts, tmp_dir, options)
     else
       nil -> {:error, "Study #{studyInstanceUid} not found"}
       error -> error
@@ -46,29 +46,30 @@ defmodule Exorthanc.Download do
     end
   end
 
-  defp dicom_stream(parts, dir, compression) do
+  defp dicom_stream(parts, dir, options) do
     parts
     |> Stream.with_index
     |> Enum.map(fn({part, index}) ->
       {_, data} = part
+      compression = Keyword.get(options, :compression)
       write_file(dir, index, data, compression)
     end)
   end
 
-  defp write_file(dir, index, data, false) do
-    filename = "#{dir}/#{index}.dcm"
-    File.write!(filename, data)
-    filename |> to_charlist
-  end
   defp write_file(dir, index, data, "j2k") do
     tmp_fn = "#{dir}/tmp#{index}.dcm"
     jpeg2k_fn = "#{dir}/#{index}.dcm"
     with :ok <- File.write(tmp_fn, data),
-         {:ok, _} <- GDCM.exec(@gdcmconv, ["-U", "--j2k", tmp_fn, jpeg2k_fn]),
-         :ok <- File.rm(tmp_fn)
+    {:ok, _} <- GDCM.exec(@gdcmconv, ["-U", "--j2k", tmp_fn, jpeg2k_fn]),
+    :ok <- File.rm(tmp_fn)
     do
       jpeg2k_fn |> to_charlist
     end
+  end
+  defp write_file(dir, index, data, nil) do
+    filename = "#{dir}/#{index}.dcm"
+    File.write!(filename, data)
+    filename |> to_charlist
   end
 
 end
