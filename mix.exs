fdd4f5f87bc0f70b8f27a24dc66090dfdb85caf0
Rodrigo Pinheiro <dap.rodrigo@gmail.com>
2019-04-03 14:02:37 -0300

    Bump_v0.1.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exorthanc.MixProject do
   def project do
     [
       app: :exorthanc,
-      version: "0.1.9",
+      version: "0.1.10",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
